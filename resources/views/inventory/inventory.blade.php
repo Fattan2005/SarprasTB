ac9fcdef1040d7821ff177b89d403d77bd483f33
aisyah <marioaistwice@gmail.com>
2022-06-21 14:39:47 +0700

    layout

---
+++ resources/views/inventory/inventory.blade.php
@@ -0,0 +1,121 @@
+@extends('main')
+@section('judul','Inventory')
+@section('isi')
+<div class="card">
+    <div class="card-body">
+        <div class="input-group input-group-sm mb-3 col-4" style="float:right">
+            <input type="search" name="search" id="search" class="form-control" placeholder="Search Barang & Jenis Barang">
+            <button class="btn btn-outline-primary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
+          </div>
+        <a href="/" class="btn btn-primary mb-4"><i
+                class="fas fa-plus"></i><span class="px-2">Tambah</span></a>
+        <a href="/exportexcelbarang" class="btn btn-success mb-4"></i><i class="fas fa-file-excel"></i><span class="px-2">Export</span></a>
+        <a href="/" class="btn btn-danger" style="margin-top:-1.5rem">Export PDF</a>
+        <table class="table table-hover table-bordered" id="barang-table">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Kode Barang</th>
+                    <th scope="col">Nama Barang</th>
+                    <th scope="col">Stok</th>
+                    <th scope="col">Jumlah Tersedia</th>
+                    <th scope="col">Jumlah Rusak</th>
+                    <th scope="col">Jumlah Terpinjam</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody class="alldata">
+                <tr>
+                  <th scope="row">1</th> 
+                  <td>lala</td>                        
+                  <td>pau</td>
+                  <td>harry</td>
+                  <td>jerry</td>
+                  <td>tom</td>
+                  <td>yeri</td>
+              </tr>
+            </tbody>
+              <tbody id="contentnya" class="searchdata"></tbody>
+        </table>
+        <div class="paginatenya mt-3">
+        {{-- {{ $barang->links() }} --}}
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xU+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/validator/13.7.0/validator.min.js" integrity="sha512-rJU+PnS2bHzDCvRGFhXouCSxf4YYaUyUfjXMHsHFfMKhWDIEBr8go2LZ2EYXOqASey1tWc2qToeOCYh9et2aGQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+    <script type="text/javascript">
+        $.ajaxSetup({ headers: { 'csrftoken' : '{{ csrf_token() }}' } });
+    </script>
+
+    <script>
+            
+      $('.delete').click(function(event) {
+      var form =  $(this).closest("form");
+      var name = $(this).data("name");
+      event.preventDefault();
+      swal({
+          title: `Are you sure you want to delete ${name}?`,
+          text: "If you delete this, it will be gone forever.",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          form.submit();
+          swal("Data berhasil di hapus", {
+                icon: "success",
+                });
+        }else 
+        {
+          swal("Data tidak jadi dihapus");
+        }
+      });
+  });
+    </script>
+
+<script>
+    $(document).ready(function(){
+     $('#search').on('keyup',function(){
+         $value= $(this).val();
+         if($value)
+         {
+          $('.alldata').hide();
+          $('.searchdata').show();
+         }
+
+         else
+         {
+          $('.alldata').show();
+          $('.searchdata').hide();
+         }
+         $.ajax({
+            url:'{{URL::to('search')}}',
+            type:"GET",
+            data:{'search':$value},
+            success:function(data){
+                $('#contentnya').html(data);
+            }
+     });
+     //end of ajax call
+    });
+    });
+</script>
+
+<script>
+    @if (Session:: has('success'))
+    toastr.success("{{ Session::get('success') }}")
+    @endif
+</script>
+
+@endpush
---
+++ resources/views/peminjaman/tambahpeminjaman.blade.php
@@ -0,0 +1,78 @@
+@extends('main')
+@section('judul','Tambah Peminjaman')
+@section('isi')
+<div class="card">
+    <div class="card-body">
+        <form method="POST" action="">
+            @csrf
+            <div class="mb-3">
+                <label class="form-label">Kode Barang</label>
+                <select required class="form-control" name="kode_barang_id" id="kode_barang_id">
+                </select>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Nama Barang</label>
+                <select required class="form-control" name="barang_id" id="barang_id">
+                </select>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <label class="form-label">Nama Peminjam</label>
+                    <input required type="text" placeholder="Nama Peminjam" class="form-control" name="nama_peminjam" required>
+                </div>
+                <div class="col">
+                    <label class="form-label">Status Peminjam</label>
+                    <select class="form-control" required name="status_peminjam" id="val_equipfc" onChange="checkOption(this)">
+                        <option selected disabled>Pillih Status Peminjam</option>
+                        <option value="Guru">Guru</option>
+                        <option value="Murid">Murid</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label class="form-label">Nama Kelas</label>
+                    <input type="text" id="val_equipnofc" placeholder="Nama Kelas" class="form-control"
+                        name="nama_kelas">
+                </div>
+                <div class="col mb-3">
+                    <label class="form-label">Jumlah Pinjam</label>
+                    <input type="number" required placeholder="Jumlah Barang Yang Di Pinjam" class="form-control"
+                        name="jumlah_pinjam">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledSelect" class="form-label">Status</label>
+                    <select id="disabledSelect" required class="form-select form-control" name="status">
+                        <option value="Dipinjam">Meminjam</option>
+                    </select>
+                </div>
+                {{-- </fieldset> --}}
+
+                <div class="col mb-3">
+                    <label class="form-label">Nama Operator</label>
+                    <select class="form-control" required name="operator_id" id="operator_id">
+                        <option disabled selected>Pilih Nama Operator</option>
+                    </select>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Keterangan</label>
+                <textarea class="form-control" required name="keterangan" placeholder="Keterangan Peminjam"
+                    style="height: 100px"></textarea>
+            </div>
+            <button class="btn btn-outline-primary" type="submit">Submit</button>
+        </form>
+    </div>
+</div>
+@endsection
+@push('scripts')
+<script type="text/javascript">
+    function checkOption(obj) {
+        var input = document.getElementById("val_equipnofc");
+        input.disabled = obj.value == "Guru";
+    }
+
+</script>
+@endpush
