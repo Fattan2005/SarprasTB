fea386e6287e4e9795bfcced4c6142a01fa1b90a
AsadCus <88858607+AsadCus@users.noreply.github.com>
2022-06-22 11:48:47 +0700

    Update Inventory.php

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    use HasFactory;
+    protected $table = "inventory";
     protected $guarded = [];
 
     public function kodebarang(){
