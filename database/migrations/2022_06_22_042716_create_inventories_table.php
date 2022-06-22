7900c8ff6faa372d5c0306329931d6b065c6474f
AsadCus <88858607+AsadCus@users.noreply.github.com>
2022-06-22 11:47:41 +0700

    Update 2022_06_22_042716_create_inventories_table.php

--- database/migrations/2022_06_22_042716_create_inventories_table.php
+++ database/migrations/2022_06_22_042716_create_inventories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('inventories', function (Blueprint $table) {
+        Schema::create('inventory', function (Blueprint $table) {
             $table->id();
             $table->foreignId('barang_id');
             $table->integer('stok');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('inventories');
+        Schema::dropIfExists('inventory');
     }
 };
